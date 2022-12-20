adec08557da4776a6587f6671fa6f27be127b131
Sergey Litvinov <slitvinov@gmail.com>
2022-12-20 15:09:48 +0100

    poc/one.c: add a reference

--- poc/one.c
+++ poc/one.c
@@ -4,26 +4,46 @@
 #include <stdlib.h>
 
 enum { n = 10, m = 2 * (n / 2 + 1) };
+#define pi 3.141592653589793238
 
 int main(int argc, char **argv) {
   int i;
+  int j;
+  double om;
   double *real;
   fftw_complex * compl ;
+  fftw_complex *refer;
   unsigned flags;
   fftw_plan plan_r2c;
   fftw_plan plan_c2r;
+  double im;
+  double re;
 
   real = fftw_alloc_real(n);
   compl = fftw_alloc_complex(m);
+  refer = fftw_alloc_complex(m);
+
   for (i = 0; i < n; i++)
     real[i] = i;
-  flags = 0;
+  flags = FFTW_DESTROY_INPUT;
   plan_r2c = fftw_plan_dft_r2c_1d(n, real, compl, flags);
   plan_c2r = fftw_plan_dft_c2r_1d(n, compl, real, flags);
   fftw_execute(plan_r2c);
 
+  for (i = 0; i < n / 2; i++) {
+    im = 0;
+    re = 0;
+    for (j = 0; j < n; j++) {
+      om = -2 * pi * j * i / n;
+      re += real[j] * cos(om);
+      im += real[j] * sin(om);
+    }
+    refer[i] = CMPLX(re, im);
+  }
+
   for (i = 0; i < n / 2; i++)
-    printf("%d %+.16e %+.16e\n", i, 2 * compl [i][0] / n, 2 * compl [i][1] / n);
+    printf("%d %+.16e %+.16e\n", i, cimag(compl [i]) - cimag(refer[i]),
+           creal(compl [i]) - creal(refer[i]));
 
   fftw_execute(plan_c2r);
   for (i = 0; i < n; i++)
