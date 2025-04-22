9b024afb0225aed53cb525e9b7ef1aa8144b2fde
Sergey Litvinov <slitvinov@gmail.com>
2025-04-22 13:35:31 +0200

    rm iflat

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ enum { N = 1 << 5, Nf = N / 2 + 1, tot = N * N * N };
     exit(1);                                                                   \
   }
 
-static fftw_plan irfftn;
 static fftw_plan rfftn;
 #define Z0 z = (i * N + j) * Nf + k
 #define Z1 z = (i * N + j) * 2 * Nf + k
@@ -121,8 +120,6 @@ int main(int argc, char **argv) {
 
   rfftn =
       fftw_mpi_plan_dft_r2c_3d(N, N, N, U, U_hat, MPI_COMM_WORLD, FFTW_MEASURE);
-  irfftn =
-      fftw_mpi_plan_dft_c2r_3d(N, N, N, U_hat, U, MPI_COMM_WORLD, FFTW_MEASURE);
   /*
   rfftn = fftw_plan_dft_r2c_3d(N, N, N, U, U_hat, FFTW_MEASURE);
   irfftn = fftw_plan_dft_c2r_3d(N, N, N, U_hat, U, FFTW_MEASURE);
@@ -273,7 +270,6 @@ int main(int argc, char **argv) {
   free(curlX);
   free(curlY);
   free(curlZ);
-  fftw_destroy_plan(irfftn);
   fftw_destroy_plan(rfftn);
   fftw_mpi_cleanup();
   MPI_Finalize();
