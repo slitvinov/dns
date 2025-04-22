d8b904ca6e1e6299103118812e8594bc3d82057a
Sergey Litvinov <slitvinov@gmail.com>
2025-04-22 13:41:34 +0200

    minor

--- main.c
+++ main.c
@@ -137,8 +137,7 @@ int main(int argc, char **argv) {
     for (j = 0; j < N; j++)
       for (k = 0; k < Nf; k++) {
         Z0;
-        dealias[z] =
-            (fabs(kx[i]) < kmax) * (fabs(kx[j]) < kmax) * (fabs(kx[k]) < kmax);
+	dealias[z] = (fabs(kx[i]) < kmax) && (fabs(kx[j]) < kmax) && (fabs(kz[k]) < kmax);
       }
 
   for (i = 0; i < N; i++)
