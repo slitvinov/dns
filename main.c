a0da7d7fd8af00c57050a30cfe25781d8119bf90
Sergey Litvinov <slitvinov@gmail.com>
2025-04-22 17:35:09 +0200

    rm rk

--- main.c
+++ main.c
@@ -24,14 +24,11 @@ static void backward(fftw_complex *U_hat, double *U) {
 
 int main(void) {
   double dx, L, s;
-  fftw_complex *curlX, *curlY, *curlZ, *dU, *dV, *dW, *P_hat, *U_hat, *U_hat0,
-      *U_hat1, *V_hat, *V_hat0, *V_hat1, *W_hat, *W_hat0, *W_hat1;
+  fftw_complex *curlX, *curlY, *curlZ, *dU, *dV, *dW, *P_hat, *U_hat, *V_hat,
+      *W_hat;
   int *dealias;
   long i, j, k, l, m;
-  int rk;
   int tstep;
-  double a[] = {1 / 6.0, 1 / 3.0, 1 / 3.0, 1 / 6.0};
-  double b[] = {0.5, 0.5, 1.0};
   double *CU, *CV, *CW, *kk, *kx, *kz, kmax, nu, dt, T, t, *U, *U_tmp, *V,
       *V_tmp, *W, *W_tmp;
   double pi = 3.141592653589793238;
@@ -40,7 +37,7 @@ int main(void) {
   feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
   nu = 0.000625;
   T = 0.1;
-  dt = 0.01;
+  dt = 0.001;
   L = 2 * pi;
   dx = L / N;
   U = fftw_alloc_real(N * N * N);
@@ -62,12 +59,6 @@ int main(void) {
   V_hat = fftw_alloc_complex(N * N * Nf);
   W_hat = fftw_alloc_complex(N * N * Nf);
   P_hat = fftw_alloc_complex(N * N * Nf);
-  U_hat0 = fftw_alloc_complex(N * N * Nf);
-  V_hat0 = fftw_alloc_complex(N * N * Nf);
-  W_hat0 = fftw_alloc_complex(N * N * Nf);
-  U_hat1 = fftw_alloc_complex(N * N * Nf);
-  V_hat1 = fftw_alloc_complex(N * N * Nf);
-  W_hat1 = fftw_alloc_complex(N * N * Nf);
   dU = fftw_alloc_complex(N * N * Nf);
   dV = fftw_alloc_complex(N * N * Nf);
   dW = fftw_alloc_complex(N * N * Nf);
@@ -124,84 +115,66 @@ int main(void) {
       s *= 0.5 * dx * dx * dx / L / L / L;
       fprintf(stderr, "k = %.16e\n", s);
     }
+    backward(U_hat, U);
+    backward(V_hat, V);
+    backward(W_hat, W);
+    for (k = 0; k < N * N * N; k++) {
+      U[k] /= tot;
+      V[k] /= tot;
+      W[k] /= tot;
+    }
+    l = 0;
+    for (i = 0; i < N; i++)
+      for (j = 0; j < N; j++)
+        for (k = 0; k < Nf; k++) {
+          curlZ[l] = I * (kx[i] * V_hat[l] - kx[j] * U_hat[l]);
+          curlY[l] = I * (kz[k] * U_hat[l] - kx[i] * W_hat[l]);
+          curlX[l] = I * (kx[j] * W_hat[l] - kz[k] * V_hat[l]);
+          l++;
+        }
+    backward(curlX, CU);
+    backward(curlY, CV);
+    backward(curlZ, CW);
+    for (k = 0; k < N * N * N; k++) {
+      CU[k] /= tot;
+      CV[k] /= tot;
+      CW[k] /= tot;
+    }
+    for (k = 0; k < N * N * N; k++) {
+      U_tmp[k] = V[k] * CW[k] - W[k] * CV[k];
+      V_tmp[k] = W[k] * CU[k] - U[k] * CW[k];
+      W_tmp[k] = U[k] * CV[k] - V[k] * CU[k];
+    }
+    forward(U_tmp, dU);
+    forward(V_tmp, dV);
+    forward(W_tmp, dW);
+
+    for (k = 0; k < N * N * Nf; k++) {
+      dU[k] *= dealias[k] * dt;
+      dV[k] *= dealias[k] * dt;
+      dW[k] *= dealias[k] * dt;
+    }
+
+    l = 0;
+    for (i = 0; i < N; i++)
+      for (j = 0; j < N; j++)
+        for (k = 0; k < Nf; k++) {
+          P_hat[l] = (dU[l] * kx[i] + dV[l] * kx[j] + dW[l] * kz[k]) / kk[l];
+          dU[l] -= P_hat[l] * kx[i] + nu * dt * kk[l] * U_hat[l];
+          dV[l] -= P_hat[l] * kx[j] + nu * dt * kk[l] * V_hat[l];
+          dW[l] -= P_hat[l] * kz[k] + nu * dt * kk[l] * W_hat[l];
+          l++;
+        }
+
+    for (k = 0; k < N * N * Nf; k++) {
+      U_hat[k] += dt * dU[k];
+      V_hat[k] += dt * dV[k];
+      W_hat[k] += dt * dW[k];
+    }
     t += dt;
     tstep++;
-    memcpy(U_hat0, U_hat, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(V_hat0, V_hat, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(W_hat0, W_hat, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(U_hat1, U_hat, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(V_hat1, V_hat, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(W_hat1, W_hat, sizeof(fftw_complex) * N * N * Nf);
-    for (rk = 0; rk < 4; rk++) {
-      if (rk > 0) {
-        backward(U_hat, U);
-        backward(V_hat, V);
-        backward(W_hat, W);
-        for (k = 0; k < N * N * N; k++) {
-          U[k] /= tot;
-          V[k] /= tot;
-          W[k] /= tot;
-        }
-      }
-      l = 0;
-      for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-          for (k = 0; k < Nf; k++) {
-            curlZ[l] = I * (kx[i] * V_hat[l] - kx[j] * U_hat[l]);
-            curlY[l] = I * (kz[k] * U_hat[l] - kx[i] * W_hat[l]);
-            curlX[l] = I * (kx[j] * W_hat[l] - kz[k] * V_hat[l]);
-            l++;
-          }
-      backward(curlX, CU);
-      backward(curlY, CV);
-      backward(curlZ, CW);
-      for (k = 0; k < N * N * N; k++) {
-        CU[k] /= tot;
-        CV[k] /= tot;
-        CW[k] /= tot;
-      }
-      for (k = 0; k < N * N * N; k++) {
-        U_tmp[k] = V[k] * CW[k] - W[k] * CV[k];
-        V_tmp[k] = W[k] * CU[k] - U[k] * CW[k];
-        W_tmp[k] = U[k] * CV[k] - V[k] * CU[k];
-      }
-      forward(U_tmp, dU);
-      forward(V_tmp, dV);
-      forward(W_tmp, dW);
-
-      for (k = 0; k < N * N * Nf; k++) {
-        dU[k] *= dealias[k] * dt;
-        dV[k] *= dealias[k] * dt;
-        dW[k] *= dealias[k] * dt;
-      }
-      l = 0;
-      for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-          for (k = 0; k < Nf; k++) {
-            P_hat[l] = (dU[l] * kx[i] + dV[l] * kx[j] + dW[l] * kz[k]) / kk[l];
-            dU[l] -= P_hat[l] * kx[i] + nu * dt * kk[l] * U_hat[l];
-            dV[l] -= P_hat[l] * kx[j] + nu * dt * kk[l] * V_hat[l];
-            dW[l] -= P_hat[l] * kz[k] + nu * dt * kk[l] * W_hat[l];
-            l++;
-          }
-
-      if (rk < 3) {
-        for (k = 0; k < N * N * Nf; k++) {
-          U_hat[k] = U_hat0[k] + b[rk] * dU[k];
-          V_hat[k] = V_hat0[k] + b[rk] * dV[k];
-          W_hat[k] = W_hat0[k] + b[rk] * dW[k];
-        }
-      }
-      for (k = 0; k < N * N * Nf; ++k) {
-        U_hat1[k] += a[rk] * dU[k];
-        V_hat1[k] += a[rk] * dV[k];
-        W_hat1[k] += a[rk] * dW[k];
-      }
-    }
-    memcpy(U_hat, U_hat1, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(V_hat, V_hat1, sizeof(fftw_complex) * N * N * Nf);
-    memcpy(W_hat, W_hat1, sizeof(fftw_complex) * N * N * Nf);
   }
+
   fftw_free(CU);
   fftw_free(curlX);
   fftw_free(curlY);
@@ -214,18 +187,12 @@ int main(void) {
   fftw_free(P_hat);
   fftw_free(U);
   fftw_free(U_hat);
-  fftw_free(U_hat0);
-  fftw_free(U_hat1);
   fftw_free(U_tmp);
   fftw_free(V);
   fftw_free(V_hat);
-  fftw_free(V_hat0);
-  fftw_free(V_hat1);
   fftw_free(V_tmp);
   fftw_free(W);
   fftw_free(W_hat);
-  fftw_free(W_hat0);
-  fftw_free(W_hat1);
   fftw_free(W_tmp);
 
   free(dealias);
