cd5f2df24577864ba08f7885a07858d1ff72669f
Sergey Litvinov <slitvinov@gmail.com>
2025-04-24 20:11:50 +0200

    up

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ for i
 do echo $i
 done | awk '{printf "%04d %.16e\n", $1, 1/$1}' | \
     xargs --process-slot-var I -n 2 -P `nproc` sh -c \
-	  'echo taskset --cpu-list $i -- -T 10 -n $1 -i tgv.raw $0' 
+	  'exec taskset --cpu-list $I ./a.out -T 10 -n $1 -i tgv.raw > $0'
