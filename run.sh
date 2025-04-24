262945adefbac8ba2561286f0f128d3aec8aed82
Sergey Litvinov <slitvinov@gmail.com>
2025-04-24 20:13:19 +0200

    up

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set 100 200 400 800 1600 3000
 for i
 do echo $i
 done | awk '{printf "%04d %.16e\n", $1, 1/$1}' | \
-    xargs --process-slot-var I -n 2 -P `nproc` sh -c \
-	  'exec taskset --cpu-list $I ./a.out -T 10 -n $1 -i tgv.raw > $0'
+    xargs --process-slot-var I -n 2 -P `nproc` sh -xc \
+	  'exec taskset --cpu-list $I ./a.out -t 10 -n $1 -i tgv.raw > $0'
