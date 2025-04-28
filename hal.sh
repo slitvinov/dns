7fb18789bcdfe9048375e66411d7490798f55c25
Sergey Litvinov <slitvinov@gmail.com>
2025-04-28 10:08:04 +0200

    up

--- hal.sh
+++ hal.sh
@@ -1,11 +1,11 @@
+python tgv.py -o tgv.raw -l 8
 cat <<! | awk '{printf "%04d %.16e\n", $1, 1/$1}' | \
     xargs --process-slot-var I -n 2 -P `nproc` sh -xc \
           '
 sleep $I
-o=$0 n=$1
-set -- 64-73 74-83 84-93 94-103 104-113 114-123
-shift $I
-exec taskset --cpu-list $1 ./dns -t 10 -n $n -s 0.0025 -i tgv.raw > $o
+i=$((64+10*I))
+j=$((64+10*(I+1)-1))
+exec taskset --cpu-list $i-$j ./dns -t 10 -n $1 -s 0.0025 -i tgv.raw > $0
 '
 100
 200
