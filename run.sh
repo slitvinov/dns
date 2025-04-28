56534bde91f1d6ecfc2176f94a79da2bfd7f9a8c
Sergey Litvinov <slitvinov@gmail.com>
2025-04-28 10:21:28 +0200

    restore run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 cat <<! | awk '{printf "%04d %.16e\n", $1, 1/$1}' | \
     xargs --process-slot-var I -n 2 -P `nproc` sh -xc \
-          '
-sleep $I
-o=$0 n=$1
-set -- 64-73 74-83 84-93 94-103 104-113 114-123
-shift $I
-exec taskset --cpu-list $1 ./dns -t 10 -n $n -s 0.0025 -i tgv.raw > $o
-'
+          'exec taskset --cpu-list $I ./dns -t 10 -n $1 -s 0.0025 -i tgv.raw > $0'
 100
 200
 400
